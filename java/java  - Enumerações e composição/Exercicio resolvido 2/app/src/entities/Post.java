d83065061313e39131340c399608c04f8f924a62
Sr. Rafael Cardoso <rafaelcsadm@hotmail.com>
2024-02-17 10:55:02 -0300

    Practicing a bit with enumeration and with string build

---
+++ java/java  - Enumerações e composição/Exercicio resolvido 2/app/src/entities/Post.java
@@ -0,0 +1,88 @@
+package entities;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Post {
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+    private Date moment;
+    private String title;
+    private String content;
+    private Integer like;
+
+    private List<Comment> comments = new ArrayList<>();
+
+
+    public Post() {
+
+    }
+
+    public Post(Date moment, String title, String content, Integer like) {
+        this.moment = moment;
+        this.title = title;
+        this.content = content;
+        this.like = like;
+    }
+
+    public Date getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Date moment) {
+        this.moment = moment;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Integer getLike() {
+        return like;
+    }
+
+    public void setLike(Integer like) {
+        this.like = like;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void addComment(Comment comment) {
+        comments.add(comment);
+    }
+
+    public void removeComment(Comment comment) {
+        comments.remove(comment);
+    }
+
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n");
+        sb.append(title).append("\n");
+        sb.append(like);
+        sb.append(" Likes - ");
+        sb.append(sdf.format(moment)).append("\n");
+        sb.append(content).append("\n");
+        sb.append("Comments:").append("\n");
+        for (Comment c : comments) {
+            sb.append(c.getText()).append("\n");
+        }
+        return sb.toString();
+    }
+}
