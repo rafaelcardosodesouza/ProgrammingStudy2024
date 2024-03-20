ad9e53914aa2e806f653707f97828253e6a8cc99
Sr. Rafael Cardoso <rafaelcsadm@hotmail.com>
2024-03-20 14:59:24 -0300

    block finally

---
+++ java/Tratamento de exceções/2/main/src/Main.java
@@ -0,0 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        File file = new File("C:\\Users\\rafae\\OneDrive\\Área de Trabalho\\estudo de programação 2024\\ProgrammingStudy2024\\java\\Tratamento de exceções\\2\\in.txt");
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.hasNextLine());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Error opening file: " + e.getMessage());
+        } finally {
+            if (sc != null) {
+                sc.close();
+
+            }
+        }
+        System.out.println("Finally block executed!!!");
+    }
+}
