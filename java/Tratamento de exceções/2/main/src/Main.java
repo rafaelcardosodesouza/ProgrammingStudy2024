6f23fe2aa8880a289328f781790257478dc5b420
Sr. Rafael Cardoso <rafaelcsadm@hotmail.com>
2024-03-22 16:19:37 -0300

    criação do exemplo de resolução um problema de tratação de exeção, tres forma de resolver, uma boa, uma ruim e uma muito ruim. Inicialmente criando a muito ruim

--- java/Tratamento de exceções/2/main/src/Main.java
+++ java/Tratamento de exceções/2/main/src/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+       /**
         File file = new File("C:\\Users\\rafae\\OneDrive\\Área de Trabalho\\estudo de programação 2024\\ProgrammingStudy2024\\java\\Tratamento de exceções\\2\\in.txt");
         Scanner sc = null;
         try {
@@ -20,5 +21,8 @@ public class Main {
             }
         }
         System.out.println("Finally block executed!!!");
-    }
+
+        **/
+
+        }
 }
