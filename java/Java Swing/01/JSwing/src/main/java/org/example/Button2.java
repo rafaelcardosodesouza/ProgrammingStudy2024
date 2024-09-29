7e6e5012bece199ad259c5ce7512e26a8a49328b
rafaRafoso <rafaelcsadm@hotmail.com>
2024-09-29 20:28:51 -0300

    estudando actionLisner

---
+++ java/Java Swing/01/JSwing/src/main/java/org/example/Button2.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Button2 implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("Botão pressionado");
+    }
+}
--- java/Java Swing/01/JSwing/src/main/java/org/example/Main.java
+++ java/Java Swing/01/JSwing/src/main/java/org/example/Main.java
@@ -3,39 +3,29 @@ package org.example;
 import javax.swing.*;
 import java.awt.*;
 
+
 public class Main {
+
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
-
-        frame.setTitle("Teste");
-
-
-
+        // Criar a janela principal
+        JFrame frame = new JFrame("Teste");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 400);
-/*
-        JButton button = new JButton("Nortth");
-        JButton button1 = new JButton("West");
-        JButton button2 = new JButton("South");
-        JButton button3 = new JButton("Center");
-        JButton button4 = new JButton("East");
-        frame.getContentPane().add(BorderLayout.NORTH, button);
-        frame.getContentPane().add(BorderLayout.WEST, button1);
-        frame.getContentPane().add(BorderLayout.SOUTH, button2);
-        frame.getContentPane().add(BorderLayout.CENTER, button3);
-        frame.getContentPane().add(BorderLayout.EAST, button4);
-*/
 
-
+        // Criar o painel para os botões
         JPanel panel = new JPanel();
 
-        panel.add(new JButton("Click me 1")).setPreferredSize(new Dimension(100, 50));
-        panel.add(new JButton("Click me 2")).setPreferredSize(new Dimension(100, 50));
+        // Criar o botão 1 e adicionar ao painel
+        JButton button1 = new JButton("Clique em mim - Botão 1");
+        button1.addActionListener(new Button1()); // Usando a classe Button1
+        panel.add(button1);
 
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-        //panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+        // Criar o botão 2 e adicionar ao painel
+        JButton button2 = new JButton("Clique em mim - Botão 2");
+        button2.addActionListener(new Button2()); // Usando a classe Button2
+        panel.add(button2);
 
-
+        // Adicionar o painel à janela
         frame.getContentPane().add(BorderLayout.EAST, panel);
         frame.setVisible(true);
     }
